31628d919c988168cb7f1a4a94b8fd8fdb76203c
Nika Paradina <nika.paradina@gmail.com>
2023-05-07 19:55:16 +0200

    df_transitional_repositories

--- main.py
+++ main.py
@@ -1,18 +1,64 @@
-import pandas as pd
+#!/usr/bin/env python3 
 
-# startig age
+import pandas as pd
+from model_fitting_gompertz import *
+import numpy as np
+import transitional_probabilities_functions as tpf
+
+
+#Setting product variables, in next iteration could be repurpused to be used as web interface
+# insurable age start
 x0=20
-# incurance petiod
+
+#Setting data for modelling
+path_death_probabilities='C:/Users/nikap/Documents/Edukacija/Aktuarstvo/Zavrsni rad/Code Repository/Zavrsni-rad/Vjerojatnost smrti populacije od kriticnih bolesti i zdravih od ostalih bolesti.xlsx'
+path_initial_Gompertz_parameteres='C:/Users/nikap/Documents/Edukacija/Aktuarstvo/Zavrsni rad/Code Repository/Zavrsni-rad/Inijalni parametri GM Modela.xlsx'
+#Setting paraemeters for transitional_probabilities_expressions
+x1=65
+x2=105
+t1=x1-x0
+t2=x2-x1
+
+
+
+#Setting insured's variable
+#age at insurance contract
+x=20
+# incurance period
 n=40
 #insured sum, in €
 S=20000
+#insured critical illnesses
+critical_illnesses=['SU','MU','R']
+#parameters of a mortality model
+mortality_model_parameters_labels=['beta1','beta2']
+
+#calculating a mortality model
+#get the number of inured illnesses
+no_illnesses=len(critical_illnesses)
+minimizer_results_list=model_fitting_gompertz(path_death_probabilities,path_initial_Gompertz_parameteres)
+
+#mortality model dataframe of parameters
+#initialize parameters list, fill it with results based retrieved from the optimisation
+mortality_params_list=[]
+for res in minimizer_results_list:
+    mortality_params_list.append(res.last_internal_values)
+    print(list(res.last_internal_values))
+
+#creating a dataframe object to hold piece-wise contant transitional intensities, data stored in a list, index added based on project assumtions
+critical_illnesses.append("ZM") # expend the list adding the row for healthy to dead from other causes probability
+mortality_params_df=pd.DataFrame(mortality_params_list,index=critical_illnesses, columns=mortality_model_parameters_labels)
+
+#pass the parameters to define transitional probabilities expressions
+transitional_probabilities_expressions=tpf.define_transitional_probabilities_functions(mortality_params_df,x0,t1)
+
+
 #piece-wise contant transition intensities dateframe
 #Build dataframe from the available diseses as index of rows and colums for sigmas
 #consider a nxn list as well
 
 
-# get the number of inured illnesses
-# call calculate mortality model
+
 # call get prevalence data - database, calulate the number of unique age groups, get age boundries
 #per prevalance date age group, 
     #call transitional_probabilities_functions function, get equatoins with parameters
--- transitional_probabilities_functions.py
+++ transitional_probabilities_functions.py
@@ -1,31 +1,51 @@
-import math as m
+import math as sm
 import scipy.integrate as integrate
 import sympy as sm
+import pandas as pd
 
-#create function, get Gompertz parameters for all diseses, get no of diseses,get X, get t return argument: pZZ and list of pZi
-#x marks the start age
-#t marks the end age
+def define_transitional_probabilities_functions (mortality_params_df_values, age, time):
+    #create function, get Gompertz parameters for all diseses, get no of diseses,get X, get t return argument: pZZ and list of pZi
+    #x marks the start age
+    #t marks the end age
 
-beta_1_Z,beta_2_Z,beta_1_i,beta_2_i,x,t,sigma_R,sigma_SU,sigma_MU,sigma_i=sm.symbols("beta_1_Z,beta_2_Z,beta_1_i,beta_2_i,x,t,sigma_R,sigma_SU,sigma_MU,sigma_i")
-const1,const2=sm.symbols("const1,const2")
+    beta_1_Z,beta_2_Z,beta_1_i,beta_2_i,x,t,sigma_R,sigma_SU,sigma_MU,sigma_i=sm.symbols("beta_1_Z,beta_2_Z,beta_1_i,beta_2_i,x,t,sigma_R,sigma_SU,sigma_MU,sigma_i")
+    const1,const2=sm.symbols("const1,const2")
 
-const1=m.exp(beta_1_Z)/beta_2_Z
-const2=m.exp(beta_1_i)/beta_2_i
+    const1=sm.exp(beta_1_Z)/beta_2_Z
+    const2=sm.exp(beta_1_i)/beta_2_i
+
+    #the probabilty of staying healthy starting from age x till x+t
+    pZZ=sm.exp(-const1*(sm.exp(beta_2_Z*x))-sm.exp(beta_2_Z*x)-(sigma_R+sigma_SU+sigma_MU)*t)
+        #the probabilty of being ill from sickness i starting from age x till x+t - check if at all needed
+    pii=sm.exp(-const1*(sm.exp(beta_2_Z*(x+t))-sm.exp(beta_2_Z*x))-const2*(sm.exp(beta_2_i*(x+t))-sm.exp(beta_2_i*x)))
+    #iterate over diseses, calulcate pZi ofr each, add to list
+    pZi=sm.exp(const1*sm.exp(beta_2_Z*x)*(1-beta_2_Z*t))*sm.exp(-const2*sm.exp(beta_2_i*(x+t))+const2*sm.exp(beta_2_i*(x+t/2))*(1-beta_2_i*t/2))*sigma_i/(-(sigma_R+sigma_SU+sigma_MU)+sm.exp(beta_1_i)*sm.exp(beta_2_i*(x+t/2)))*sm.exp((sm.exp(beta_1_i)*sm.exp(beta_2_i*(x+t/2))-(sigma_R+sigma_SU+sigma_MU))*t-1)
+    #set Gomepert values to get the return pZi 
+    
+    transitional_probability_expressions_pii=pd.Series()
+    transitional_probability_expressions_pZi=pd.Series()
+    transitional_probability_expressions_pZZ=pd.Series()
+    
+    #supstituting varibales with numerical values
+    for row in mortality_params_df_values.itertuples():
+        if row[0]!="ZM":
+            transitional_probability_expressions_row_pii=pd.Series(data=pii.subs(beta_2_Z,mortality_params_df_values["beta2"][row[0]]).subs(beta_1_Z,mortality_params_df_values["beta1"][row[0]]).subs(x,age).subs(t,time), index=[row[0]])
+            transitional_probability_expressions_pii=pd.concat([transitional_probability_expressions_pii,transitional_probability_expressions_row_pii], axis=0)
+            transitional_probability_expressions_row_pZi=pd.Series(data=pZi.subs(beta_2_Z,mortality_params_df_values["beta2"][row[0]]).subs(beta_1_Z,mortality_params_df_values["beta1"][row[0]]).subs(x,age).subs(t,time), index=[row[0]])
+            transitional_probability_expressions_pZi=pd.concat([transitional_probability_expressions_pZi,transitional_probability_expressions_row_pZi], axis=0)
+        else:
+            transitional_probability_expressions_pZZ=pd.Series(data=pZZ.subs(beta_2_Z,mortality_params_df_values["beta2"][row[0]]).subs(beta_1_Z,mortality_params_df_values["beta1"][row[0]]).subs(x,age).subs(t,time), index=[row[0]])
+
+    transitional_probability_expressions_all=pd.concat([transitional_probability_expressions_pii,transitional_probability_expressions_pZi,transitional_probability_expressions_pZZ], axis=0)
+  
+    print(transitional_probability_expressions_all)
+    pass
+
+   
+  
 
 
-#the probabilty of staying healthy starting from age x till x+t
-pZZ=m.exp(-const1*(m.exp(beta_2_Z*x))-m.exp(beta_2_Z*x)-(sigma_R+sigma_SU+sigma_MU)*t)
-... #set Gomepert values to get the return pZZ
-
-#the probabilty of staying ill from sickness i starting from age x till x+t - check if at all needed
-pii=m.exp(-const1*(m.exp(beta_2_Z*(x+t))-m.exp(beta_2_Z*x))-const2*(m.exp(beta_2_i*(x+t))-m.exp(beta_2_i*x)))
+    #return list of pZi
 
 
-#the probabilty of being healthy starting from age x, and being ill from sickness i at x+t
-
-#iterate over diseses, calulcate pZi ofr each, add to list
-
-pZi=m.exp(const1*m.exp(beta_2_Z*x)*(1-beta_2_Z*t))*m.exp(-const2*m.exp(beta_2_i*(x+t))+const2*m.exp(beta_2_i*(x+t/2))*(1-beta_2_i*t/2))*sigma_i/(-(sigma_R+sigma_SU+sigma_MU)+m.exp(beta_1_i)*m.exp(beta_2_i*(x+t/2)))*m.exp((m.exp(beta_1_i)*m.exp(beta_2_i*(x+t/2))-(sigma_R+sigma_SU+sigma_MU))*t-1)
-... #set Gomepert values to get the return pZi 
-
-#return list of pZi
+    pass
