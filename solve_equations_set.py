b11f0cf4cd84e5d15d71262f232f6b3d3c713566
Nika Paradina <nika.paradina@gmail.com>
2023-05-14 20:45:09 +0200

    solve_EQ_no_fix

---
+++ solve_equations_set.py
@@ -0,0 +1,24 @@
+import math as m
+import scipy.integrate as integrate
+import sympy as sm
+import prevalence_rates_equations as pre
+import datetime as dt
+
+
+def solve_equations_set(equation_list):
+    equation_tuple=tuple(equation_list)
+    sigma_R,sigma_SU,sigma_MU=sm.symbols("sigma_R,sigma_SU,sigma_MU", real=True)
+    #get unknow parameters
+    y0=equation_list[0]
+    y1=equation_list[1]
+    y2=equation_list[1]
+    # print(y0)
+    # print(y1)
+    # print(y2)
+    ct = dt.datetime.now()
+    print("current time:-", ct)
+    sol = sm.solve((y0,y1,y2),(sigma_R,sigma_SU,sigma_MU),simplify=False)
+    ct = dt.datetime.now()
+    print("current time:-", ct)
+    print(sol)
+    pass
