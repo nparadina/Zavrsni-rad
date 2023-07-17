0a27d3e9bb9f9ff5cbe7a3a8882e8ad0aae7b088
Nika Paradina <nika.paradina@gmail.com>
2023-07-17 07:55:02 +0200

    Changes to expressions, corrections

--- temp4_fsolve.py
+++ temp4_fsolve.py
@@ -8,9 +8,9 @@ def equations(z):
     sigma_MU=z[1]
     sigma_R=z[2]
     f=np.empty(3)
-    f[0] = -0.356651831549143*sigma_SU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/((-sigma_MU - sigma_R - sigma_SU + 1.8122035744266e-154)*(0.356651831549143*sigma_R*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 9.22287914393683e-129) + 0.356651831549143*sigma_SU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 1.8122035744266e-154) + 0.155401054964061*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU) + 0.719582172729324)) + 1.54
-    f[1] = -0.356651831549143*sigma_R*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/((-sigma_MU - sigma_R - sigma_SU + 9.22287914393683e-129)*(0.356651831549143*sigma_R*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 9.22287914393683e-129) + 0.356651831549143*sigma_SU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 1.8122035744266e-154) + 0.155401054964061*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU) + 0.719582172729324)) + 0.37
-    f[2] = 0.3 - 0.719582172729324/(0.356651831549143*sigma_R*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU))/(-sigma_MU - sigma_R - sigma_SU + 9.22287914393683e-129) + 0.356651831549143*sigma_SU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 1.8122035744266e-154) + 0.155401054964061*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU) + 0.71958217272932
+    f[0] = -0.262617004746481*sigma_SU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/((-sigma_MU - sigma_R - sigma_SU + 0.000931334226877499)*(0.260884896827677*sigma_MU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 0.00170434902663555) + 0.256617389979587*sigma_R*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 0.00363094118807797) + 0.262617004746481*sigma_SU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 0.000931334226877499) + 0.719582172729324*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU))) + 1.54
+    f[1] = -0.260884896827677*sigma_MU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/((-sigma_MU - sigma_R - sigma_SU + 0.00170434902663555)*(0.260884896827677*sigma_MU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 0.00170434902663555) + 0.256617389979587*sigma_R*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 0.00363094118807797) + 0.262617004746481*sigma_SU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 0.000931334226877499) + 0.719582172729324*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU))) + 0.3
+    f[2] =  -0.256617389979587*sigma_R*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/((-sigma_MU - sigma_R - sigma_SU + 0.00363094118807797)*(0.260884896827677*sigma_MU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 0.00170434902663555) + 0.256617389979587*sigma_R*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 0.00363094118807797) + 0.262617004746481*sigma_SU*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU)/(-sigma_MU - sigma_R - sigma_SU + 0.000931334226877499) + 0.719582172729324*np.exp(-45*sigma_MU - 45*sigma_R - 45*sigma_SU))) + 0.37
     return f
 
 myGuess=np.array([0.0000813,0.0000202,0.000384])
@@ -20,10 +20,12 @@ counter=0
 ct1 = dt.datetime.now()
 print(ct1)
 
-z= fsolve(equations, myGuess)
+z=fsolve(equations, myGuess)
 
 # while np.any(z<=0):
 #     counter+=1
+#     #myGuess+=0.0000001
+#     myGuess=z
 #     z= fsolve(equations, myGuess)
 
 ct2=dt.datetime.now()
