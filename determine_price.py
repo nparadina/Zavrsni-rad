fe8f521d8c10416a089be9e35e33a24a680a02c1
Nika Paradina <nika.paradina@gmail.com>
2023-08-16 12:01:51 +0200

    saving calculated probabilties and determine_price

---
+++ determine_price.py
@@ -0,0 +1,3 @@
+def determine_price(age_of_entry,policy_duration,initial_stepwise_intensity,second_stepwise_intensity,age_group_limits):
+    print("Entering price determination")
+    pass
--- prevalence_rates_equations.py
+++ prevalence_rates_equations.py
@@ -50,7 +50,7 @@ def prevalence_rates_equations (transitional_probabilities_expressions_initial_a
     exp_df_second_age_group=pd.concat([exp_df_second_age_group,pZZ_second_age_group_temp_df], ignore_index=True)
     exp_df_second_age_group=exp_df_second_age_group.set_index('illness')
 
-#Creates an sympy equation for prevalence rate, with initial age group; sympify converts to a Sympy objecct
+#Creates an sympy equation for prevalence rate, with initial age group; sympify converts to a Sympy object
     pZZ_initial=sm.sympify(exp_df_initial.at['pZZ','pZZ'])
     #a list to hold all CI pZi_initials for the sum needed in equation
     sum_pZi_initials_list=[]
@@ -72,6 +72,7 @@ def prevalence_rates_equations (transitional_probabilities_expressions_initial_a
         new_exp_i=f_i-pZi_initial/(pZZ_initial+sum_pZi_initials)
         new_exp_i=new_exp_i.subs(f_i,average_prevalance_rates_all.at[illness,'20-64'])
         eq_list_initial.append(new_exp_i)
+        
         #Back when sm.nsolve was in play and Equatoins were needed
         #eq_list_initial.append(sm.Eq(new_exp_i,0))
         
@@ -80,19 +81,17 @@ def prevalence_rates_equations (transitional_probabilities_expressions_initial_a
     myGuess_initial=[0.00076082, 0.00051096, 0.00394945]
     initialStepwiseProbabilityObject.fsolve_stepwise(myGuess_initial)
 
+#Setting initializsation to calculate stepwise probabilities for second age group
 #Using the calculated initial stepwise intensities, calculate the probabilities for the age group x0 to x2. 
-#Using the calculated initial stepwise intensities, build a dataframe object initial_calculated_probabilities to be used in premium calculation
     sum_pZi_up_to_second_age_group_list=[]
     pZi_up_to_second_age_group_series=pd.Series(dtype=str)
     pZZ_calculated_initial=pZZ_initial.subs(sigma_R,initialStepwiseProbabilityObject.sol[0]).subs(sigma_SU,initialStepwiseProbabilityObject.sol[1]).subs(sigma_MU,initialStepwiseProbabilityObject.sol[2])
-    pZZ_calculated_initial_df_data=pd.DataFrame(data={"pii":'',"pZi":'',"pZZ":pZZ_calculated_initial}, index=['pZZ'])
     pZZ_up_to_second_age_group=pZZ_calculated_initial*sm.sympify(exp_df_second_age_group.at['pZZ','pZZ'])
-    
-
-#Creating a dataframe object to use later in calucations of premiums
+#Using the calculated initial stepwise intensities, build a dataframe object initial_calculated_probabilities; to be used for evaluations of calculation 
+    pZZ_calculated_initial_df_data=pd.DataFrame(data={"pii":'',"pZi":'',"pZZ":pZZ_calculated_initial}, index=['pZZ'])
+#Creating a dataframe object for all probabilties, initial and second age group; to be used for evaluations of calculation 
     initial_calculated_probabilities=pd.DataFrame(columns=['pii','pZi','pZZ'])
     second_age_group_calculated_probabilities=pd.DataFrame(columns=['pii','pZi','pZZ'])
-
     initial_calculated_probabilities=pd.concat([pZZ_calculated_initial_df_data,initial_calculated_probabilities])
 
     for illness in CRITICAL_ILLNESSES:
@@ -102,11 +101,11 @@ def prevalence_rates_equations (transitional_probabilities_expressions_initial_a
         pZi_calculated_initial_df_data=pd.DataFrame(data={"pii":'',"pZi":pZi_calculated_initial,"pZZ":''}, index=[illness])
         initial_calculated_probabilities=pd.concat([pZi_calculated_initial_df_data,initial_calculated_probabilities])
         #pii_initial is indenpented of sigmas, therefore the same as pii_calculated_initial would be
-        pii_initial=sm.sympify(exp_df_second_age_group.at[illness,'pii'].loc[~exp_df_second_age_group.at[illness,'pii'].eq('')].at[illness])
+        pii_initial=sm.sympify(exp_df_initial.at[illness,'pii'].loc[~exp_df_second_age_group.at[illness,'pii'].eq('')].at[illness])
         pii_calculated_initial_df_data=pd.DataFrame(data={"pii":pii_initial,"pZi":'',"pZZ":''}, index=[illness])
         initial_calculated_probabilities=pd.concat([pii_calculated_initial_df_data,initial_calculated_probabilities])
-       
-        #the probabilities from x0+t1 up to x0+t2 we have already calculated in exp_df_second_age_group
+
+        #the probabilities from x0+t1 up to x0+t2 we have already calculated in exp_df_second_age_group, logic beneath calculates the x0 up to x0+t1+t2 probablity
         pZi_up_to_second_age_group=pZi_calculated_initial*sm.sympify(exp_df_second_age_group.at[illness,'pii'].loc[~exp_df_second_age_group.at[illness,'pii'].eq('')].at[illness])+pZZ_calculated_initial*sm.sympify(exp_df_second_age_group.at[illness,'pZi'].loc[~exp_df_second_age_group.at[illness,'pZi'].eq('')].at[illness])
         #store the pZis in a list to calculate the sum later
         sum_pZi_up_to_second_age_group_list.append(pZi_up_to_second_age_group)
@@ -127,6 +126,22 @@ def prevalence_rates_equations (transitional_probabilities_expressions_initial_a
     
     secondStepwiseProbabilityObject=ses.StepwiseProbabilty(eq_list_second_age_group)
     secondStepwiseProbabilityObject.fsolve_stepwise(myGuess_second_age_group)
+    
+    #calculate second age group probabilties based on the sigmas (aka stepwise transitinal intensities) determined in secondStepwiseProbabilityObject
+    pZZ_second_age_group=sm.sympify(exp_df_second_age_group.at['pZZ','pZZ'])
+    pZZ_calculated_second_age_group=pZZ_second_age_group.subs(sigma_R,secondStepwiseProbabilityObject.sol[0]).subs(sigma_SU,secondStepwiseProbabilityObject.sol[1]).subs(sigma_MU,secondStepwiseProbabilityObject.sol[2])
+    pZZ_calculated__second_age_group_df_data=pd.DataFrame(data={"pii":'',"pZi":'',"pZZ":pZZ_calculated_second_age_group}, index=['pZZ'])
+    second_age_group_calculated_probabilities=pd.concat([pZZ_calculated__second_age_group_df_data,second_age_group_calculated_probabilities])
+    #iterate ove disease to calculate the probablities in the second age group
+    for illness in CRITICAL_ILLNESSES:
+        #select the probability value from exp_df_second_age_group that mathches the illness at location other than (~) empty (eq(''))
+        pZi_second_age_group=sm.sympify(exp_df_second_age_group.at[illness,'pZi'].loc[~exp_df_second_age_group.at[illness,'pZi'].eq('')].at[illness])
+        pZi_calculated_second_age_group=pZi_second_age_group.subs(sigma_R,secondStepwiseProbabilityObject.sol[0]).subs(sigma_SU,secondStepwiseProbabilityObject.sol[1]).subs(sigma_MU,secondStepwiseProbabilityObject.sol[2])
+        pZi_calculated_second_age_group_df_data=pd.DataFrame(data={"pii":'',"pZi":pZi_calculated_second_age_group,"pZZ":''}, index=[illness])
+        second_age_group_calculated_probabilities=pd.concat([pZi_calculated_second_age_group_df_data,second_age_group_calculated_probabilities])
+        pii_second_age_group=sm.sympify(exp_df_second_age_group.at[illness,'pii'].loc[~exp_df_second_age_group.at[illness,'pii'].eq('')].at[illness])
+        pii_calculated_second_age_group_df_data=pd.DataFrame(data={"pii":pii_second_age_group,"pZi":'',"pZZ":''}, index=[illness])
+        second_age_group_calculated_probabilities=pd.concat([pii_calculated_second_age_group_df_data,second_age_group_calculated_probabilities])
 
     return initialStepwiseProbabilityObject,secondStepwiseProbabilityObject
      
