15d832a82cfe1ec6c6de40676564e4da05d5b8bd
Nika Paradina <nika.paradina@gmail.com>
2024-04-22 07:20:15 +0200

    no changes

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ sci=25000
 s=25000
 
 #flag and value of added mortality, if used set to True
-include_added_mortality=True
+include_added_mortality=False
 gamma=0.25
 
 #Setting data for modelling, Excel Sheets
